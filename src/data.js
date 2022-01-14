a81a99be4a9d898a53ed3064cdf6934f0a6d4685
Mércio Andrade Filho <merciofilho@gmail.com>
2022-01-13 21:32:42 -0300

    update default data

--- src/data.js
+++ src/data.js
@@ -5,9 +5,7 @@ export const defaultUser = {
   username: "username",
   name: "name",
   profile_image:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF",
-  // profile_image:
-  // "https://instagram.com/static/images/anonymousUser.jpg/23e7b3b2a737.jpg"
+    "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/avatar.png"
 };
 
 export function getDefaultUser() {
@@ -16,7 +14,7 @@ export function getDefaultUser() {
     username: "username",
     name: "name",
     profile_image:
-      "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF",
+      "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/avatar.png"
   };
 }
 
@@ -26,9 +24,9 @@ export const defaultPost = {
   caption: `<span class="">Do you know the 10 JavaScript concepts you need to learn React? 🤔⚛️👇<br>•<br>•<br>👉 Get the FREE cheatsheet to learn them now: bit.ly/10-js-tips 🔥</span>`,
   user: defaultUser,
   media:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/80126161_2456912044637768_8188145638093587415_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_cat=108&_nc_ohc=GAkCBeSLLnwAX_Xn25U&oh=58af570f5ad051b24583c388b946784a&oe=5EA44484",
+    "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/post.jpeg",
   comments: [],
-  created_at: "2020-02-28T03:08:14.522421+00:00",
+  created_at: "2020-02-28T03:08:14.522421+00:00"
 };
 
 export function getDefaultPost() {
@@ -38,9 +36,9 @@ export function getDefaultPost() {
     caption: `<span class="">Do you know the 10 JavaScript concepts you need to learn React? 🤔⚛️👇<br>•<br>•<br>👉 Get the FREE cheatsheet to learn them now: bit.ly/10-js-tips 🔥</span>`,
     user: defaultUser,
     media:
-      "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/80126161_2456912044637768_8188145638093587415_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_cat=108&_nc_ohc=GAkCBeSLLnwAX_Xn25U&oh=58af570f5ad051b24583c388b946784a&oe=5EA44484",
+      "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/post.jpeg",
     comments: [],
-    created_at: "2020-02-28T03:08:14.522421+00:00",
+    created_at: "2020-02-28T03:08:14.522421+00:00"
   };
 }
 
@@ -49,15 +47,15 @@ export const defaultNotifications = [
     id: uuid(),
     type: "follow",
     user: defaultUser,
-    created_at: "2020-02-29T03:08:14.522421+00:00",
+    created_at: "2020-02-29T03:08:14.522421+00:00"
   },
   {
     id: uuid(),
     type: "like",
     user: defaultUser,
     post: defaultPost,
-    created_at: "2020-02-29T03:08:14.522421+00:00",
-  },
+    created_at: "2020-02-29T03:08:14.522421+00:00"
+  }
 ];
 
 export const defaultCurrentUser = {
@@ -65,12 +63,12 @@ export const defaultCurrentUser = {
   username: "me",
   name: "myself",
   profile_image:
-    "https://scontent-ort2-2.cdninstagram.com/v/t51.2885-19/s150x150/82559664_3161307737426774_8687807477812559872_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_ohc=llb3VCRb-mkAX_XTEi3&oh=64f0323db646c01299e513e3337b83ff&oe=5E8620BF",
-  website: "https://react12.io",
+    "https://reedbarger.nyc3.digitaloceanspaces.com/reactbootcamp/avatar.png",
+  website: "https://reactbootcamp.com",
   email: "me@gmail.com",
   bio: "This is my bio",
   phone_number: "555-555-5555",
-  posts: [],
+  posts: Array.from({ length: 10 }, () => getDefaultPost()),
   followers: [defaultUser],
-  following: [defaultUser],
+  following: [defaultUser]
 };
